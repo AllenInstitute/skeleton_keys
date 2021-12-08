b02d16c98da6184e9ad737eb37275b6c1534eccc
Nathan Gouwens <nathang@alleninstitute.org>
2021-12-08 12:08:44 -0800

    Add script to calculate depth histograms from layer-aligned swcs

---
+++ skeleton_keys/cmds/depth_profiles_from_aligned_swcs.py
@@ -0,0 +1,92 @@
+import argschema as ags
+import os
+import numpy as np
+import pandas as pd
+import json
+from skeleton_keys.io import load_swc_as_dataframe
+
+
+class ProfilesFromAlignedSwcsParameters(ags.ArgSchema):
+    specimen_id_file = ags.fields.InputFile(
+        description="File with specimen IDs on each line"
+    )
+    swc_dir = ags.fields.InputDir(
+        description="Directory with layer-aligned SWC files"
+    )
+    layer_depths_file = ags.fields.InputFile(
+        description="JSON file with layer depths; used to establish bins for profile histogram",
+        default="avg_layer_depths.json")
+    bin_size = ags.fields.Float(description="bin size, in microns", default=5.0)
+    below_wm = ags.fields.Float(description="extent below white matter to include, in microns", default=200.0)
+    output_hist_file = ags.fields.OutputFile(
+        description="output CSV file for depth profiles",
+        default="aligned_histograms_corrected.csv")
+    output_soma_file = ags.fields.OutputFile(
+        description="output CSV file for soma depths",
+        default="aligned_soma_depths_corrected.csv")
+
+
+def main():
+    module = ags.ArgSchemaParser(schema_type=ProfilesFromAlignedSwcsParameters)
+
+    # Load the specimen IDs
+    specimen_id_file = module.args['specimen_id_file']
+    specimen_ids = np.loadtxt(specimen_id_file, dtype=int)
+
+    # Load the layer info
+    layer_depths_file = module.args['layer_depths_file']
+    with open(layer_depths_file, "r") as f:
+        avg_layer_depths = json.load(f)
+
+    # Get directory with layer-aligned SWCs
+    swc_dir = module.args['swc_dir']
+
+    # Set up bins for profile histograms
+    bin_size = module.args['bin_size']
+    below_wm = module.args['below_wm']
+    bottom_edge = avg_layer_depths["wm"] + below_wm
+    bins = np.arange(0, bottom_edge, bin_size)
+
+    hist_record_list = []
+    soma_record_list = []
+    for spec_id in specimen_ids:
+        # Load individual morphology
+        swc_file = os.path.join(swc_dir, f"{spec_id}.swc")
+        if not os.path.exists(swc_file):
+            print(f"No SWC file found in {swc_dir} for {spec_id}")
+            continue
+
+        morph_df = load_swc_as_dataframe(swc_file)
+
+        # Create the layer-aligned histogram
+        result = {}
+        node_types = [2, 3, 4]
+        for nt in node_types:
+            subset_df = morph_df.loc[morph_df['type'] == nt, :]
+            y_values = -subset_df['y'].values
+            hist_values, _ = np.histogram(y_values, bins=bins)
+            result[nt] = hist_values
+
+        flat_result_dict = {"specimen_id": spec_id}
+        for nt in result:
+            for i in range(len(result[nt])):
+                k = f"{nt}_{i}"
+                flat_result_dict[k] = result[nt][i]
+        hist_record_list.append(flat_result_dict)
+
+        # Pull out the soma depths
+        soma_y = morph_df.loc[morph_df['type'] == 1, 'y'].values[0]
+        soma_record_list.append({
+            'specimen_id': spec_id,
+            'soma_distance_from_pia': -soma_y,
+        })
+
+    # Save results
+    output_hist_file = module.args['output_hist_file']
+    output_soma_file = module.args['output_soma_file']
+    pd.DataFrame.from_records(hist_record_list).fillna(0.).set_index("specimen_id").to_csv(output_hist_file)
+    pd.DataFrame.from_records(soma_record_list).fillna(0.).set_index("specimen_id").to_csv(output_soma_file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ skeleton_keys/io.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def load_swc_as_dataframe(swc_file):
+    """ Load a morphology SWC file into a pandas DataFrame.
+
+    The dataframe contains the columns:
+        - ID : node identifier
+        - type : node type, which could be
+            1 = soma
+            2 = axon
+            3 = basal dendrite (or generic dendrite)
+            4 = apical dendrite
+        - x : x-coordinate
+        - y : y-coordinate
+        - z : z-coordinate
+        - r : radius
+        - parent_id : identifier of parent node
+
+    Parameters
+    ----------
+    swc_file : str
+        File path of SWC file
+
+    Returns
+    -------
+    df : DataFrame
+        Dataframe with morphology information
+    """
+    return pd.read_table(
+        swc_file,
+        sep=" ",
+        comment="#",
+        names=["id", "type", "x", "y", "z", "r", "parent_id"],
+    )

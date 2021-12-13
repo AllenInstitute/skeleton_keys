ad34a57e0182c5fbf0e925e77fa277f48e40cc5c
Forrest Collman <forrest.collman@gmail.com>
2021-12-13 17:51:03 -0500

    adding morp feature task

--- skeleton_keys/tasks.py
+++ skeleton_keys/tasks.py
@@ -1,4 +1,5 @@
 from taskqueue import queueable
+from skeleton_keys.cmds import process_morphology_features
 from skeleton_keys.cmds.layer_aligned_swc import (
     LayerAlignedSwcSchema,
     main as layer_align_main,
@@ -6,6 +7,10 @@ from skeleton_keys.cmds.layer_aligned_swc import (
 from skeleton_keys.cmds.upright_corrected_swc import (
     UprightCorrectedSwcSchema,
     main as upright_main,
+)
+from skeleton_keys.cmds.process_morphology_features import (
+    ProcessMorphologyFeaturesParameters,
+    main as morph_feat_main,
 )
 import argschema
 
@@ -69,3 +74,44 @@ def upright_correct_cell(
         schema_type=UprightCorrectedSwcSchema, input_data=input_data, args=[]
     )
     upright_main(module.args)
+
+
+@queueable
+def extract_morphology_features(
+    specimen_id,
+    aligned_depth_profile_file,
+    aligned_soma_file,
+    output_file,
+    swc_dir=None,
+    swc_paths_file=None,
+    layer_list=None,
+    analyze_axon=False,
+    analyze_basal_dendrite=False,
+    analyze_apical_dendrite=False,
+    analyze_basal_dendrite_depth=False,
+    axon_depth_profile_loadings_file=None,
+    basal_dendrite_depth_profile_loadings_file=None,
+    apical_dendrite_depth_profile_loadings_file=None,
+):
+    input_data = {
+        "specimen_id": specimen_id,
+        "aligned_depth_profile_file": aligned_depth_profile_file,
+        "aligned_soma_file": aligned_depth_profile_file,
+        "swc_dir": swc_dir,
+        "swc_paths_file": swc_paths_file,
+        "layer_list": layer_list,
+        "analyze_axon": analyze_axon,
+        "analyze_basal_dendrite": analyze_basal_dendrite,
+        "analyze_apical_dendrite": analyze_apical_dendrite,
+        "analyze_basal_dendrite_depth": analyze_basal_dendrite_depth,
+        "axon_depth_profile_loadings_file": axon_depth_profile_loadings_file,
+        "basal_dendrite_depth_profile_loadings_file": basal_dendrite_depth_profile_loadings_file,
+        "apical_dendrite_depth_profile_loadings_file": apical_dendrite_depth_profile_loadings_file,
+        "output_file": output_file,
+    }
+
+    input_data = {k: v for k, v in input_data.items() if v is not None}
+    module = argschema.ArgSchemaParser(
+        schema_type=ProcessMorphologyFeaturesParameters, input_data=input_data, args=[]
+    )
+    morph_feat_main(module.args)

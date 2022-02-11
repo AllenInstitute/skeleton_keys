86372a197031cdb1814c758949adeaad472849dd
Forrest Collman <forrest.collman@gmail.com>
2022-02-11 14:51:49 -0800

    Revert "Cloudfiles emj"

--- skeleton_keys/cmds/layer_aligned_swc.py
+++ skeleton_keys/cmds/layer_aligned_swc.py
@@ -30,16 +30,15 @@ from skeleton_keys.drawings import (
 )
 from skeleton_keys.upright import corrected_without_uprighting_morph
 from skeleton_keys.layer_alignment import layer_aligned_y_values
-from skeleton_keys.io import read_json_file
-from skeleton_keys import cloudfields
+
 
 class LayerAlignedSwcSchema(ags.ArgSchema):
     specimen_id = ags.fields.Integer(description="Specimen ID")
-    swc_path = cloudfields.InputFile(
+    swc_path = ags.fields.InputFile(
         description="path to SWC file (optional)", default=None, allow_none=True
     )
-    layer_depths_file = cloudfields.InputFile(default="avg_layer_depths.json")
-    output_file = cloudfields.OutputFile(default="output.swc")
+    layer_depths_file = ags.fields.InputFile(default="avg_layer_depths.json")
+    output_file = ags.fields.OutputFile(default="output.swc")
     correct_for_shrinkage = ags.fields.Boolean(
         default=True,
         description="Whether to correct for shrinkage",
@@ -48,17 +47,17 @@ class LayerAlignedSwcSchema(ags.ArgSchema):
         default=True,
         description="Whether to correct for slice angle",
     )
-    surface_and_layers_file = cloudfields.InputFile(
+    surface_and_layers_file = ags.fields.InputFile(
         description="JSON file with surface and layer polygon paths",
         default=None,
         allow_none=True,
     )
-    closest_surface_voxel_file = cloudfields.InputFile(
+    closest_surface_voxel_file = ags.fields.InputFile(
         default=None,
         allow_none=True,
         description="Closest surface voxel reference HDF5 file for slice angle calculation",
     )
-    surface_paths_file = cloudfields.InputFile(
+    surface_paths_file = ags.fields.InputFile(
         default=None,
         allow_none=True,
         description="Surface paths (streamlines) HDF5 file for slice angle calculation",
@@ -80,15 +79,16 @@ def main(args):
         swc_path = swc_paths_from_database([specimen_id])[specimen_id]
 
     # Load the reference layer depths
-    avg_layer_depths = read_json_file(args["layer_depths_file"])
+    with open(args["layer_depths_file"], "r") as f:
+        avg_layer_depths = json.load(f)
 
     layer_list = args["layer_list"]
 
     # Get pia, white matter, soma, and layers
     surface_and_layers_file = args["surface_and_layers_file"]
     if surface_and_layers_file is not None:
-        surfaces_and_paths = read_json_file(surface_and_layers_file)
-
+        with open(surface_and_layers_file, "r") as f:
+            surfaces_and_paths = json.load(f)
         pia_surface = surfaces_and_paths["pia_path"]
         wm_surface = surfaces_and_paths["wm_path"]
         soma_drawing = surfaces_and_paths["soma_path"]
--- skeleton_keys/cmds/postprocess_features.py
+++ skeleton_keys/cmds/postprocess_features.py
@@ -1,30 +1,20 @@
 import logging
 import re
-import glob
 import numpy as np
 import pandas as pd
 import argschema as ags
-from skeleton_keys import cloudfields
-from skeleton_keys.io import read_json_file, get_path_files, read_csv
+
 
 class PostprocessFeaturesParameters(ags.ArgSchema):
-    input_file_directory = cloudfields.InputDir(
-        default=None,
-        required=False,
-        allow_none=True,
-        description="Directory containing long-form CSV files of morphology features",
-    )
     input_files = ags.fields.List(
-        cloudfields.InputFile(),
-        default=[],
-        required=False,
+        ags.fields.InputFile(),
         cli_as_single_argument=True,
         description="Long-form CSV files of morphology features",
     )
-    wide_normalized_output_file = cloudfields.OutputFile(
+    wide_normalized_output_file = ags.fields.OutputFile(
         description="Wide-form CSV of normalized features",
     )
-    wide_unnormalized_output_file = cloudfields.OutputFile(
+    wide_unnormalized_output_file = ags.fields.OutputFile(
         description="Wide-form CSV of un-normalized features",
     )
 
@@ -54,20 +44,8 @@ def _natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
 
 def main(args):
     df_list = []
-    if args["input_file_directory"] is not None:
-        input_files = get_path_files(args["input_file_directory"], regex_template=".csv$")
-        print(f'Found {len(input_files)} files in the input directory')
-    else:
-        input_files = []
-    
-    for fn in args["input_files"]:
-        input_files.append(fn)
-    
-    if len(input_files) == 0:
-        raise ValueError("No input files found!")
-    
-    for filename in input_files:
-        df = read_csv(filename, index_col=0)
+    for filename in args["input_files"]:
+        df = pd.read_csv(filename, index_col=0)
         df_list.append(df)
 
     morph_df = pd.concat(df_list)

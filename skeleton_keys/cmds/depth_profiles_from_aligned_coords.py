4b4739b1d008e9f3f24559c69a45af43ae880ff3
Nathan Gouwens <nathang@alleninstitute.org>
2022-10-18 11:03:42 -0700

    Add scripts for coordinate layer alignment after refactoring out morph-specific code

---
+++ skeleton_keys/cmds/depth_profiles_from_aligned_coords.py
@@ -0,0 +1,123 @@
+import os
+import json
+import argschema as ags
+import numpy as np
+import pandas as pd
+from skeleton_keys.io import load_default_layer_template
+
+
+class ProfilesFromAlignedCoordsParameters(ags.ArgSchema):
+    coordinate_file = ags.fields.InputFile(
+        description="File with specimen IDs on each line"
+    )
+    depth_label = ags.fields.String(
+        default="y",
+        description="label of column with aligned depth values",
+    )
+    index_label = ags.fields.String(
+        default=None,
+        allow_none=True,
+        description="label of column to split values across rows"
+    )
+    hist_split_label = ags.fields.String(
+        default=None,
+        allow_none=True,
+        description="label of column to split values across multiple sets of columns"
+    )
+    layer_depths_file = ags.fields.InputFile(
+        description="JSON file with layer depths; used to establish bins for profile histogram",
+        default=None, allow_none=True)
+    bin_size = ags.fields.Float(description="bin size, in microns", default=5.0)
+    below_wm = ags.fields.Float(description="extent below white matter to include, in microns", default=200.0)
+    output_hist_file = ags.fields.OutputFile(
+        description="output CSV file for depth profiles",
+        default="aligned_histograms_corrected.csv")
+
+
+
+
+def main(args):
+    # Load the coordinates
+    coordinate_file = args['coordinate_file']
+    coord_df = pd.read_csv(coordinate_file)
+
+
+    # Load the layer info
+    layer_depths_file = args['layer_depths_file']
+    if layer_depths_file:
+        with open(layer_depths_file, "r") as f:
+            avg_layer_depths = json.load(f)
+    else:
+        avg_layer_depths = load_default_layer_template()
+
+    # Set up bins for profile histograms
+    bin_size = args['bin_size']
+    below_wm = args['below_wm']
+    bottom_edge = avg_layer_depths["wm"] + below_wm
+    bins = np.arange(0, bottom_edge, bin_size)
+
+    record_list = []
+
+    depth_label = args['depth_label']
+    index_group_label = args['index_label']
+    col_group_label = args['hist_split_label']
+
+    if index_group_label is None:
+        if col_group_label is None:
+            # Calculating a single histogram for all values
+            col_prefix = "bin"
+            names = [f"{col_prefix}_{i}" for i in range(len(bins) - 1)]
+            y_values = -coord_df[depth_label].values
+            hist_values, _ = np.histogram(y_values, bins=bins)
+            out_df = pd.DataFrame(hist_values, index=names).T
+        else:
+            names = []
+            hist_list = []
+            for col_ind, col_group in coord_df.groupby(col_group_label):
+                names += [f"{col_ind}_{i}" for i in range(len(bins) - 1)]
+                y_values = -col_group[depth_label].values
+                hist_values, _ = np.histogram(y_values, bins=bins)
+                hist_list.append(hist_values)
+            out_df = pd.DataFrame(np.hstack(hist_list), index=names).T
+
+    else:
+        if col_group_label is None:
+            col_prefix = "bin"
+            names = [f"{col_prefix}_{i}" for i in range(len(bins) - 1)]
+            group_dict = {}
+            for row_ind, row_group in coord_df.groupby(index_group_label):
+                y_values = -row_group[depth_label].values
+                hist_values, _ = np.histogram(y_values, bins=bins)
+                group_dict[row_ind] = hist_values
+            out_df = pd.DataFrame(group_dict, index=names).T.rename_axis(index_group_label).reset_index()
+        else:
+            group_dict = {}
+            names = []
+            # Collect all the histograms
+            for col_ind, col_group in coord_df.groupby(col_group_label):
+                names += [f"{col_ind}_{i}" for i in range(len(bins) - 1)]
+                for row_ind, row_group in col_group.groupby(index_group_label):
+                    if row_ind not in group_dict:
+                        group_dict[row_ind] = []
+                    y_values = -row_group[depth_label].values
+                    hist_values, _ = np.histogram(y_values, bins=bins)
+                    group_dict[row_ind].append(hist_values)
+
+            # Merge horizontally before constructing dataframe
+            for row_ind in group_dict:
+                group_dict[row_ind] = np.hstack(group_dict[row_ind])
+
+            out_df = pd.DataFrame(group_dict, index=names).T.rename_axis(index_group_label).reset_index()
+
+
+    out_df.to_csv(args['output_hist_file'], index=False)
+
+
+def console_script():
+    module = ags.ArgSchemaParser(schema_type=ProfilesFromAlignedCoordsParameters)
+    main(module.args)
+
+
+if __name__ == "__main__":
+    module = ags.ArgSchemaParser(schema_type=ProfilesFromAlignedCoordsParameters)
+    main(module.args)
--- skeleton_keys/layer_alignment.py
+++ skeleton_keys/layer_alignment.py
@@ -1,4 +1,5 @@
 import geopandas
+import logging
 import shapely
 import shapely.vectorized
 import numpy as np
@@ -6,53 +7,166 @@ from scipy.interpolate import RegularGridInterpolator
 from shapely.geometry import LineString
 from typing import Dict, List, Tuple, Optional
 from neuron_morphology.layered_point_depths.__main__ import tuplize, setup_interpolator
+from tqdm import tqdm
 
 
-def cortex_thickness_aligned_y_values(morph, avg_layer_depths,
+def cortex_thickness_aligned_y_values_for_morph(morph, avg_layer_depths,
         depth_field):
+    """Point depths for a morphology aligned to the full thickness of cortex.
+
+    Layer-specific information is not used. Zero is pia, negative values are
+    below pia.
+
+    Parameters
+    ----------
+    morph : Morphology
+        Morphology object
+    avg_layer_depths : dict
+        Dictionary of distances from top of layer to pia (in microns)
+    depth_field : xarray DataArray
+        Data array of the depth field
+
+    Returns
+    -------
+    depths_dict : dict
+        Dictionary of node ids (keys) and cortex-thickness aligned depths (values)
+
+    """
     nodes_ids, nodes_x, nodes_y = zip(*[(n["id"], n["x"], n["y"]) for n in morph.nodes()])
 
     nodes_ids = np.array(nodes_ids)
     nodes_x = np.array(nodes_x)
     nodes_y = np.array(nodes_y)
+
+    depths = cortex_thickness_aligned_y_values(nodes_x, nodes_y,
+        avg_layer_depths, depth_field)
+
+    nan_mask = np.isnan(depths)
+    logging.info(f"NaN y-values: {np.sum(nan_mask)}")
+    return dict(zip(nodes_ids[~nan_mask], depths[~nan_mask]))
+
+
+
+def cortex_thickness_aligned_y_values(x, y, avg_layer_depths,
+        depth_field):
+    """Point depths for a x, y coordinates aligned to the full thickness of cortex.
+
+    Layer-specific information is not used. Zero is pia, negative values are
+    below pia.
+
+    Parameters
+    ----------
+    x : array (n_points, )
+        Array of x-coordinates
+    y : array (n_points, )
+        Array of y-coordinates
+    avg_layer_depths : dict
+        Dictionary of distances from top of layer to pia (in microns) (must
+        include "wm" (white matter) as a key)
+    depth_field : xarray DataArray
+        Data array of the depth field
+
+    Returns
+    -------
+    depths : array
+        Array of cortex-thickness aligned depth coordinates
+    """
 
     # Interpolators for navigating fields - gives nan values for points outside field
     depth_interp = setup_interpolator(
         depth_field, None, method="linear",
         bounds_error=False, fill_value=None)
 
-    pos = np.column_stack((nodes_x, nodes_y))
+    pos = np.column_stack((x, y))
     rel_depths = depth_interp(pos)
 
     total_thickness = avg_layer_depths['wm']
     depths = -total_thickness * (1 - rel_depths)
-    nan_mask = np.isnan(depths)
-    print("NaN y-values: ", np.sum(nan_mask))
-    return dict(zip(nodes_ids[~nan_mask], depths[~nan_mask]))
+
+    return depths
 
 
-def layer_aligned_y_values(morph, avg_layer_depths, layer_list,
+def layer_aligned_y_values_for_morph(morph, avg_layer_depths, layer_list,
         depth_field, gradient_field, snapped_polys_surfs):
-    nodes_ids, nodes_x, nodes_y = zip(*[(n["id"], n["x"], n["y"]) for n in morph.nodes()])
+    """Point depths for a morphology aligned to a set of layer thicknesses.
 
-    avg_side_depths = layer_side_depths(avg_layer_depths, layer_list)
+    Parameters
+    ----------
+    morph : Morphology
+        Morphology object
+    avg_layer_depths : dict
+        Dictionary of distances from top of layer to pia (in microns)
+    layer_list : dict
+        List of layers
+    depth_field : xarray DataArray
+        Data array of the depth field
+    gradient_field : xarray DataArray
+        Data array of the depth field
+    snapped_polys_surfs : dict
+        Dictionary of surface and layer drawings
+
+    Returns
+    -------
+    y_values : dict
+        Dictionary of node ids (keys) and layer-aligned depths (values)
+    """
+
+    nodes_ids, nodes_x, nodes_y = zip(*[(n["id"], n["x"], n["y"]) for n in morph.nodes()])
 
     nodes_ids = np.array(nodes_ids)
     nodes_x = np.array(nodes_x)
     nodes_y = np.array(nodes_y)
 
+    y_values_arr = layer_aligned_y_values(nodes_x, nodes_y,
+        avg_layer_depths, layer_list,
+        depth_field, gradient_field, snapped_polys_surfs
+    )
+
+    y_values = {n_id: y for n_id, y in zip(nodes_ids, y_values_arr) if not np.isnan(y)}
+    return y_values
+
+
+def layer_aligned_y_values(x, y, avg_layer_depths, layer_list,
+        depth_field, gradient_field, snapped_polys_surfs):
+    """Point depths for a morphology aligned to a set of layer thicknesses.
+
+    Parameters
+    ----------
+    x : array (n_points, )
+        Array of x-coordinates
+    y : array (n_points, )
+        Array of y-coordinates
+    avg_layer_depths : dict
+        Dictionary of distances from top of layer to pia (in microns)
+    layer_list : dict
+        List of layers
+    depth_field : xarray DataArray
+        Data array of the depth field
+    gradient_field : xarray DataArray
+        Data array of the depth field
+    snapped_polys_surfs : dict
+        Dictionary of surface and layer drawings
+
+    Returns
+    -------
+    y_values : array (n_points, )
+        Dictionary of node ids (keys) and layer-aligned depths (values)
+    """
+    # Depths of each side of layers
+    avg_side_depths = layer_side_depths(avg_layer_depths, layer_list)
+
     # Assign nodes to layers
     layer_containing_masks = layer_locations(
-        snapped_polys_surfs["polygons"], nodes_x, nodes_y)
+        snapped_polys_surfs["polygons"], x, y)
 
     # QC that most nodes are actually in layers
     running_total = 0
     for k in layer_containing_masks:
         mask = layer_containing_masks[k]
         n_in_layer = np.sum(mask)
-        print(k, n_in_layer)
+        logging.info(f"{k}: {n_in_layer}")
         running_total += n_in_layer
-    print(running_total, "out of", len(layer_containing_masks[k]), "points located in layers")
+    logging.info(f"{running_total} out of {len(layer_containing_masks[k])} points located in layers")
 
     # Interpolators for navigating fields
     depth_interp = setup_interpolator_without_nan(
@@ -68,10 +182,13 @@ def layer_aligned_y_values(morph, avg_layer_depths, layer_list,
     # Find intersection with layer edges to calculate fraction within layer
     surfs_dict = {s["name"]: shapely.geometry.LineString(s["path"]) for s in snapped_polys_surfs["surfaces"]}
 
-    y_values = {}
+    all_pos = np.array([x, y]).T
+    y_values = np.zeros_like(y)
+    mask_inside_layers = np.zeros_like(y).astype(bool)
     for layer in layer_containing_masks:
-        print("processing points in", layer)
+        logging.info(f"processing points in {layer}")
         mask = layer_containing_masks[layer]
+        mask_inside_layers = mask_inside_layers | mask
         if np.sum(mask) == 0:
             continue
 
@@ -80,56 +197,52 @@ def layer_aligned_y_values(morph, avg_layer_depths, layer_list,
         pia_surf = surfs_dict[layer + "_pia"]
         wm_surf = surfs_dict[layer + "_wm"]
 
-        nodes = morph.nodes(node_ids=nodes_ids[mask])
-        layer_fracs = nodes_fractions_within_layer(nodes, pia_surf, wm_surf, depth_interp, dx_interp, dy_interp)
+        pos = all_pos[mask, :]
+        layer_fracs = fractions_within_layer(pos, pia_surf, wm_surf, depth_interp, dx_interp, dy_interp)
         layer_y_values = -avg_pia_side - (avg_wm_side - avg_pia_side) * layer_fracs
-        layer_y_values = dict(zip(
-            [n['id'] for n in nodes],
-            layer_y_values
-        ))
+        y_values[mask] = layer_y_values
 
-        y_values.update(layer_y_values)
-
-    ids_outside_layers = [i for i in nodes_ids if i not in y_values]
+    mask_outside_layers = ~mask_inside_layers
     pia_coords = list(surfs_dict["pia"].coords)
     wm_coords = list(surfs_dict["wm"].coords)
     pia = surfs_dict["pia"]
     wm = surfs_dict["wm"]
 
     pts_in_wm = 0
-    print("processing points outside layers")
-    for node in morph.nodes(node_ids=ids_outside_layers):
+    logging.info("processing points outside layers")
+    for i, (pt_x, pt_y) in enumerate(all_pos[mask_outside_layers, :]):
         # check if it's within the wm
-        pia_tri = shapely.geometry.Polygon([pia_coords[0], pia_coords[-1], (node["x"], node["y"])])
-        wm_tri = shapely.geometry.Polygon([wm_coords[0], wm_coords[-1], (node["x"], node["y"])])
-        node_pt = shapely.geometry.Point((node["x"], node["y"]))
+        pia_tri = shapely.geometry.Polygon([pia_coords[0], pia_coords[-1], (pt_x, pt_y)])
+        wm_tri = shapely.geometry.Polygon([wm_coords[0], wm_coords[-1], (pt_x, pt_y)])
+        node_pt = shapely.geometry.Point((pt_x, pt_y))
         if pia_tri.intersection(wm_tri).area > 0 and (pia_tri.area > wm_tri.area):
             # likely below white matter border, but check if it's closer to wm than pia
             if pia.distance(node_pt) < wm.distance(node_pt):
                 # not in white matter since closer to pia
+                y_values[mask_outside_layers][i] = np.nan
                 continue
         elif wm.convex_hull.contains(node_pt):
             # in white matter, but where triangles don't overlap
             pass
         else:
             # not in white matter
+            y_values[mask_outside_layers][i] = np.nan
             continue
         pts_in_wm += 1
-        y_values[node["id"]] = -avg_layer_depths["wm"] - node_pt.distance(wm)
+        y_values[mask_outside_layers][i] = -avg_layer_depths["wm"] - node_pt.distance(wm)
 
-    print("points in wm", pts_in_wm)
-    unassigned = len(nodes_ids) - pts_in_wm - running_total
+    logging.info(f"points in wm: {pts_in_wm}")
+    unassigned = len(y) - pts_in_wm - running_total
     if unassigned > 0:
-        print(f"{unassigned} points not assigned new y-value")
+        logging.info(f"{unassigned} points not assigned new y-value")
 
-    print(f"{np.sum(np.isnan([y for k, y in y_values.items()]))} NaN y-values")
+    logging.info(f"{np.sum(np.isnan(y_values))} NaN y-values")
 
     return y_values
 
 
-def nodes_fractions_within_layer(nodes, pia_side_surf, wm_side_surf,
+def fractions_within_layer(pos, pia_side_surf, wm_side_surf,
         depth_interp, dx_interp, dy_interp, step_size=1.0, max_iter=1000):
-    pos = np.array(list(zip(*[(n['x'], n['y']) for n in nodes]))).T
     depths = depth_interp(pos)
 
     local_pia_side_depths = step_all_nodes(
@@ -152,44 +265,46 @@ def step_all_nodes(pos, depth_interp, dx_interp, dy_interp, surf, step_size, max
 
     surf_df = geopandas.GeoDataFrame({"name": ['surf'], "geometry": [surf]})
 
-    for _ in range(max_iter):
-        dx = dx_interp(cur_pos)
-        dy = dy_interp(cur_pos)
-        base_step = np.vstack([dx, dy]).T
-        base_step = base_step / np.linalg.norm(base_step, axis=1)[:, np.newaxis]
-        step = step_size * adaptive_scale[:, np.newaxis] * base_step
-        step[finished] = 0 * step[finished]
-        next_pos = cur_pos + step
-        ray_list = [shapely.geometry.LineString([tuple(pos[i, :]), tuple(next_pos[i, :])]) for i in range(cur_pos.shape[0]) if not finished[i]]
-        ray_df = geopandas.GeoDataFrame({"ind": orig_ind[~finished], "geometry": ray_list})
-        intersect_df = surf_df.sjoin(ray_df, how='inner', predicate='intersects')
+    with tqdm(total=pos.shape[0]) as pbar:
+        for _ in range(max_iter):
+            dx = dx_interp(cur_pos)
+            dy = dy_interp(cur_pos)
+            base_step = np.vstack([dx, dy]).T
+            base_step = base_step / np.linalg.norm(base_step, axis=1)[:, np.newaxis]
+            step = step_size * adaptive_scale[:, np.newaxis] * base_step
+            step[finished] = 0 * step[finished]
+            next_pos = cur_pos + step
+            ray_list = [shapely.geometry.LineString([tuple(pos[i, :]), tuple(next_pos[i, :])]) for i in range(cur_pos.shape[0]) if not finished[i]]
+            ray_df = geopandas.GeoDataFrame({"ind": orig_ind[~finished], "geometry": ray_list})
+            intersect_df = surf_df.sjoin(ray_df, how='inner', predicate='intersects')
 
-        # handle matches
-        for ind in intersect_df['ind']:
-            if adaptive_scale[ind] > 1:
-                # Reduce scale and try again from same point
-                adaptive_scale[ind] /= 2
-                next_pos[ind, :] = cur_pos[ind, :]
-                continue
-            ray = ray_df.set_index("ind").at[ind, "geometry"]
-            intersection = ray.intersection(surf)
-            if intersection.geom_type == "MultiPoint":
-                cur_pt = shapely.geometry.Point(cur_pos[ind, :])
-                dist = np.inf
-                for test_pt in intersection.geoms:
-                    test_dist = cur_pt.distance(test_pt)
-                    if test_dist < dist:
-                        dist = test_dist
-                        closest_pt = test_pt
-                intersection_pt = list(closest_pt.coords)
-            else:
-                intersection_pt = list(intersection.coords)
-            depths_of_intersections[ind] = float(depth_interp(intersection_pt[0]))
-            finished[ind] = True
-
-        if np.all(finished):
-            break
-        cur_pos = next_pos
+            # handle matches
+            n_already_finished = finished.sum()
+            for ind in intersect_df['ind']:
+                if adaptive_scale[ind] > 1:
+                    # Reduce scale and try again from same point
+                    adaptive_scale[ind] /= 2
+                    next_pos[ind, :] = cur_pos[ind, :]
+                    continue
+                ray = ray_df.set_index("ind").at[ind, "geometry"]
+                intersection = ray.intersection(surf)
+                if intersection.geom_type == "MultiPoint":
+                    cur_pt = shapely.geometry.Point(cur_pos[ind, :])
+                    dist = np.inf
+                    for test_pt in intersection.geoms:
+                        test_dist = cur_pt.distance(test_pt)
+                        if test_dist < dist:
+                            dist = test_dist
+                            closest_pt = test_pt
+                    intersection_pt = list(closest_pt.coords)
+                else:
+                    intersection_pt = list(intersection.coords)
+                depths_of_intersections[ind] = float(depth_interp(intersection_pt[0]))
+                finished[ind] = True
+            pbar.update(finished.sum() - n_already_finished)
+            if np.all(finished):
+                break
+            cur_pos = next_pos
     return depths_of_intersections
 
 

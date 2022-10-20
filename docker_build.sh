640fef1f79736cbec38e54c537e08c10ec7f2744
Forrest Collman <forrest.collman@gmail.com>
2022-10-20 14:36:21 -0700

    fixing docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,1 @@
-docker build -t fcollman/skelkeys:$1 . --build-arg GITHUB_TOKEN=$GITHUB_TOKEN
+docker buildx build --no-cache --platform linux/amd64 -t fcollman/skelkeys:$1 . --build-arg GITHUB_TOKEN=$GITHUB_TOKEN

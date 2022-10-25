85a2c69c0660c22d029763dca0ba8c2b6dab6d62
emjoyce <=>
2022-10-25 16:43:54 -0700

    adding mshr workaround for feature creation tasks

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM continuumio/miniconda3:4.10.3
 RUN apt-get update && apt-get install -y build-essential && rm -rf /var/lib/apt/lists/*
 RUN conda config --set channel_priority strict 
-RUN conda install -y -c conda-forge rtree==0.9.7 fenics==2019.1.0 python==3.8 mshr==2019.1.0 hdf5==1.10.6 h5py==2.10.0 Jinja2==2.11.3
+RUN conda install -y -c conda-forge rtree==0.9.7 fenics==2019.1.0 python==3.8 hdf5==1.10.6 h5py==2.10.0 Jinja2==2.11.3
 RUN pip install git+https://github.com/AllenInstitute/AllenSDK.git
-RUN pip install git+https://github.com/AllenInstitute/neuron_morphology@cloudfiles
-ARG GITHUB_TOKEN
+RUN pip install git+https://github.com/AllenInstitute/neuron_morphology@science_staging
 WORKDIR /usr/local/src
-RUN git clone https://${GITHUB_TOKEN}@github.com/AllenInstitute/ccf_streamlines.git &&\
+RUN git clone https://github.com/AllenInstitute/ccf_streamlines.git &&\
     pip install ./ccf_streamlines &&\
     rm -rf /usr/local/src/ccf_streamlines
 WORKDIR /usr/local/src/skeleton_keys
--- skeleton_keys/hist_tasks.py
+++ skeleton_keys/hist_tasks.py
@@ -3,6 +3,11 @@ from skeleton_keys.cmds.depth_profiles_from_aligned_swcs import (
     main as depth_profile_main
 )
 
+from skeleton_keys.cmds.process_morphology_features import (
+    ProcessMorphologyFeaturesParameters,
+    main as morph_feat_main,
+)
+
 import argschema
 from taskqueue import queueable
 
@@ -31,3 +36,43 @@ def create_layer_histograms(
         schema_type=ProfilesFromAlignedSwcsParameters, input_data=input_data, args=[]
     )
     depth_profile_main(module)
+
+@queueable
+def extract_morphology_features(
+    specimen_id_file,
+    aligned_depth_profile_file,
+    aligned_soma_file,
+    output_file,
+    swc_dir=None,
+    swc_paths_file=None,
+    layer_list=None,
+    analyze_axon=False,
+    analyze_basal_dendrite=False,
+    analyze_apical_dendrite=False,
+    analyze_basal_dendrite_depth=False,
+    axon_depth_profile_loadings_file=None,
+    basal_dendrite_depth_profile_loadings_file=None,
+    apical_dendrite_depth_profile_loadings_file=None,
+):
+    input_data = {
+        "specimen_id_file": specimen_id_file,
+        "aligned_depth_profile_file": aligned_depth_profile_file,
+        "aligned_soma_file": aligned_soma_file,
+        "swc_dir": swc_dir,
+        "swc_paths_file": swc_paths_file,
+        "layer_list": layer_list,
+        "analyze_axon": analyze_axon,
+        "analyze_basal_dendrite": analyze_basal_dendrite,
+        "analyze_apical_dendrite": analyze_apical_dendrite,
+        "analyze_basal_dendrite_depth": analyze_basal_dendrite_depth,
+        "axon_depth_profile_loadings_file": axon_depth_profile_loadings_file,
+        "basal_dendrite_depth_profile_loadings_file": basal_dendrite_depth_profile_loadings_file,
+        "apical_dendrite_depth_profile_loadings_file": apical_dendrite_depth_profile_loadings_file,
+        "output_file": output_file,
+    }
+
+    input_data = {k: v for k, v in input_data.items() if v is not None}
+    module = argschema.ArgSchemaParser(
+        schema_type=ProcessMorphologyFeaturesParameters, input_data=input_data, args=[]
+    )
+    morph_feat_main(module.args)

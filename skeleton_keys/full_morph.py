12402e1dd1571442877f1d44e7e02d77d9966e35
Nathan Gouwens <nathang@alleninstitute.org>
2023-03-09 10:21:17 -0800

    Add methods for using surface curvature to find 2D drawings for full morph cells

--- skeleton_keys/full_morph.py
+++ skeleton_keys/full_morph.py
@@ -1,4 +1,5 @@
 import logging
+import vtk
 import numpy as np
 from ccf_streamlines.angle import find_closest_streamline
 from neuron_morphology.transforms.affine_transform import (
@@ -8,9 +9,11 @@ from allensdk.core.reference_space_cache import ReferenceSpaceCache
 from skimage.measure import find_contours
 from scipy.interpolate import interpn
 from scipy.spatial.distance import euclidean
+from scipy.spatial.transform import Rotation
+from vtk.util.numpy_support import vtk_to_numpy
 
 
-def rotate_morphology_for_drawings(morphology, angle_for_atlas_slice, base_orientation='coronal'):
+def rotate_morphology_for_drawings_by_angle(morphology, angle_for_atlas_slice, base_orientation='coronal'):
     """ Rotate a whole-brain morphology to line up with angled atlas image
 
     In the CCF, x is anterior/posterior, y is dorsal/ventral, and z is left/right
@@ -58,7 +61,90 @@ def rotate_morphology_for_drawings(morphology, angle_for_atlas_slice, base_orien
     else:
         raise ValueError("base_orientation must be 'coronal' or 'parasagittal'")
 
-    # center on soma before tilt
+    rotation_tilt_matrix = rotation_from_angle(-angle_for_atlas_slice, axis=rot_axis)
+    morphology = _rotate_morphology_around_soma(morphology, rotation_tilt_matrix)
+    morphology = _swap_morphology_coordinates(morphology, coordinate_swap_matrix)
+
+    return morphology
+
+
+def rotate_morphology_for_drawings_by_rotation(morphology, q):
+    """ Rotate a whole-brain morphology to line up with angled atlas image
+
+    In the CCF, x is anterior/posterior, y is dorsal/ventral, and z is left/right
+
+    In a near-coronal slice, though, the first dimension is (primarily) dorsal/ventral and the
+    second is left/right.
+
+    In a near-parasaggital slice, the first dimension is dorsal/ventral and the second is
+    anterior/posterior.
+
+    This function both tilts the morphology to line up
+    with the atlas slice and swaps the coordinates so that the dimensions are
+    correctly matched up.
+
+    Parameters
+    ----------
+    morphology : Morphology
+        Full brain morphology, aligned to CCF (micron scale)
+    q : Rotation
+        SciPy rotation object that generated the angled atlas image
+
+    Returns
+    -------
+    morphology : Morphology
+        Rotated morphology with soma in original location
+
+    """
+
+    M = q.inv().as_matrix()
+    morphology = _rotate_morphology_around_soma(morphology, M)
+
+    coordinate_swap_matrix = np.array([
+        [0, 1, 0],
+        [1, 0, 0],
+        [0, 0, 1],
+    ])
+    morphology = _swap_morphology_coordinates(morphology, coordinate_swap_matrix)
+
+    return morphology
+
+
+def align_morphology_to_drawings(morphology, atlas_slice, atlas_resolution=10.):
+    """ Move the soma to the center of a set of drawings
+
+    Parameters
+    ----------
+    morphology : Morphology
+        Full brain morphology, aligned to CCF (micron scale)
+    atlas_slice : 2D array
+        Angled atlas slice with CCF region IDs as values
+    atlas_resolution : float, default 10
+        Voxel size of atlas volume (microns)
+
+    Returns
+    -------
+    aligned_morphology : Morphology
+        Translated morphology
+    """
+    # Align morphology to center of drawings
+    soma_morph = morphology.get_soma()
+    translation = np.array([
+        -soma_morph['x'] + atlas_resolution * atlas_slice.shape[0] / 2,
+        -soma_morph["y"] + atlas_resolution * atlas_slice.shape[1] / 2,
+        -soma_morph["z"]
+    ])
+    translation_affine = affine_from_translation(translation)
+    T_translate = AffineTransform(translation_affine)
+    T_translate.transform_morphology(morphology)
+
+    return morphology
+
+
+def _rotate_morphology_around_soma(morphology, M):
+    """Rotates morphology around its soma but maintains original position"""
+
+    # center on soma before rotation
     soma_morph = morphology.get_soma()
     translation_to_origin = np.array([-soma_morph['x'], -soma_morph["y"], -soma_morph["z"]])
     translation_back_to_soma_location = -translation_to_origin
@@ -68,8 +154,7 @@ def rotate_morphology_for_drawings(morphology, angle_for_atlas_slice, base_orien
     T_translate.transform_morphology(morphology)
 
     # Rotate
-    rotation_tilt_matrix = rotation_from_angle(-angle_for_atlas_slice, axis=rot_axis)
-    rotation_tilt_affine = affine_from_transform_translation(transform=rotation_tilt_matrix)
+    rotation_tilt_affine = affine_from_transform_translation(transform=M)
     T_rotate = AffineTransform(rotation_tilt_affine)
     T_rotate.transform_morphology(morphology)
 
@@ -78,8 +163,14 @@ def rotate_morphology_for_drawings(morphology, angle_for_atlas_slice, base_orien
     T_translate = AffineTransform(translation_affine)
     T_translate.transform_morphology(morphology)
 
+    return morphology
+
+
+def _swap_morphology_coordinates(morphology, M):
+    """Use matrix to switch coordinates"""
+
     # Swap coordinates
-    swap_affine = affine_from_transform_translation(transform=coordinate_swap_matrix)
+    swap_affine = affine_from_transform_translation(transform=M)
     T_swap = AffineTransform(swap_affine)
     T_swap.transform_morphology(morphology)
 
@@ -212,6 +303,138 @@ def angled_atlas_slice_for_morph(morph, atlas_volume,
         return atlas_slice, angle_rad, base_orientation
     else:
         return atlas_slice
+
+
+def min_curvature_atlas_slice_for_morph(morph, atlas_volume,
+        closest_surface_voxel_reference_file, surface_paths_file,
+        pia_curvature_surface_file, wm_curvature_surface_file,
+        atlas_resolution=10.):
+    """ Create an angled atlas slice lined up with a cell's streamline
+
+    The soma location of the morphology will be in the center of the returned slice.
+
+    Parameters
+    ----------
+    morph : Morphology
+        Full brain morphology, aligned to CCF (micron scale)
+    atlas_volume : 3D array
+        Region-annotated CCF atlas volume
+    closest_surface_voxel_reference_file : str
+        Closest surface voxel reference HDF5 file path for angle calculation
+    surface_paths_file : str
+        Surface paths (streamlines) HDF5 file path for slice angle calculation
+    pia_curvature_surface_file : str
+        VTP file path for pia surface curvature values
+    wm_curvature_surface_file : str
+        VTP file path for white matter surface curvature values
+    atlas_resolution : float, default 10
+        Voxel size of atlas volume (microns)
+
+    Returns
+    -------
+    atlas_slice : 2D array
+        Angled atlas slice with CCF region IDs as values
+    rot : Rotation object
+        Rotation object that produced the slice
+    """
+
+    morph_soma = morph.get_soma()
+    soma_coords = np.array([morph_soma['x'], morph_soma['y'], morph_soma['z']])
+
+    # Find streamline for cell (coordinates in um)
+    morph_streamline = find_closest_streamline(
+        soma_coords, closest_surface_voxel_reference_file, surface_paths_file)
+
+    # Convert to mm
+    top_streamline_coords_mm = morph_streamline[0, :] / 1000.
+    bottom_streamline_coords_mm = morph_streamline[-1, :] / 1000.
+
+    # Load the surfaces with curvature information
+    # (surface coordinates are in mm)
+    pia_reader = vtk.vtkXMLPolyDataReader()
+    pia_reader.SetFileName(pia_curvature_surface_file)
+    pia_reader.Update()
+    pia_surf = pia_reader.GetOutput()
+
+    wm_reader = vtk.vtkXMLPolyDataReader()
+    wm_reader.SetFileName(wm_curvature_surface_file)
+    wm_reader.Update()
+    wm_surf = wm_reader.GetOutput()
+
+    # Get the vectors in direction of maximum curvature (ie those normal to minimum curvature)
+    pia_t1_arr = vtk_to_numpy(pia_surf.GetCellData().GetArray("t_1"))
+    wm_t1_arr = vtk_to_numpy(wm_surf.GetCellData().GetArray("t_1"))
+
+    # Get the max (kappa1) & min (kappa2) curvature values
+    pia_kappa1_arr = vtk_to_numpy(pia_surf.GetCellData().GetArray("kappa_1"))
+    wm_kappa1_arr = vtk_to_numpy(wm_surf.GetCellData().GetArray("kappa_1"))
+    pia_kappa2_arr = vtk_to_numpy(pia_surf.GetCellData().GetArray("kappa_2"))
+    wm_kappa2_arr = vtk_to_numpy(wm_surf.GetCellData().GetArray("kappa_2"))
+
+    pia_delta_kappa_arr = pia_kappa1_arr - pia_kappa2_arr
+    wm_delta_kappa_arr = wm_kappa1_arr - wm_kappa2_arr
+
+    # Find the values of curvature nearest top & bottom of streamline
+    pia_point_id = pia_surf.FindPoint(top_streamline_coords_mm)
+    wm_point_id = wm_surf.FindPoint(bottom_streamline_coords_mm)
+    pia_cell_id_list = vtk.vtkIdList()
+    wm_cell_id_list = vtk.vtkIdList()
+    pia_surf.GetPointCells(pia_point_id, pia_cell_id_list)
+    wm_surf.GetPointCells(wm_point_id, wm_cell_id_list)
+
+    # Just use the first cell found
+    pia_cell_idx = pia_cell_id_list.GetId(0)
+    wm_cell_idx = wm_cell_id_list.GetId(0)
+
+    # Pick the end that has bigger potential increase in curvature if changed from its minimum direction
+    if pia_delta_kappa_arr[pia_cell_idx] >= wm_delta_kappa_arr[wm_cell_idx]:
+        norm_vec = pia_t1_arr[pia_cell_idx]
+    else:
+        norm_vec = wm_t1_arr[wm_cell_idx]
+
+    # Set up the reference mesh (parasagittal orientation) centered on the soma
+    soma_coords_for_atlas = soma_coords / atlas_resolution
+    ref_norm = np.array([0, 0, 1])
+    slice_grid = np.meshgrid(
+        np.arange(0, atlas_volume.shape[0], 1) + soma_coords_for_atlas[0] - atlas_volume.shape[0] / 2,
+        np.arange(0, atlas_volume.shape[1], 1) + soma_coords_for_atlas[1] - atlas_volume.shape[1] / 2,
+        soma_coords_for_atlas[2],
+    )
+    reshape_size = (atlas_volume.shape[1], atlas_volume.shape[0])
+    mesh_coords = np.array([
+        slice_grid[0].flatten(),
+        slice_grid[1].flatten(),
+        slice_grid[2].flatten()
+    ])
+
+    # Calculate the rotation by quaternion
+    a = np.cross(ref_norm, norm_vec)
+    w = 1 + np.dot(ref_norm, norm_vec)
+    q = Rotation.from_quat([a[0], a[1], a[2], w])
+
+    # Rotate the mesh
+    M = q.as_matrix()
+    rot_mesh_coords = (M @ (mesh_coords - soma_coords_for_atlas[:, np.newaxis])).T + soma_coords_for_atlas
+
+    # Get an interpolated slice through the mesh. Use nearest method since
+    # averaging ID values has no meaning.
+    atlas_points = (
+        np.arange(atlas_volume.shape[0], dtype=float),
+        np.arange(atlas_volume.shape[1], dtype=float),
+        np.arange(atlas_volume.shape[2], dtype=float)
+    )
+    atlas_slice = interpn(
+        atlas_points,
+        atlas_volume,
+        rot_mesh_coords,
+        method='nearest',
+        fill_value=0,
+        bounds_error=False,
+    ).astype(int)
+
+    atlas_slice = atlas_slice.reshape(reshape_size)
+
+    return atlas_slice, q
 
 
 def select_structures_of_interest(atlas_image, structure_list, tree=None):

10b4c3f829aed729e00297e51043052a82d40acb
emjoyce <emilybemily55@gmail.com>
2024-01-09 17:56:34 -0800

    add try except to spec id iterations in depth prof

--- skeleton_keys/cmds/depth_profiles_from_aligned_swcs.py
+++ skeleton_keys/cmds/depth_profiles_from_aligned_swcs.py
@@ -65,38 +65,40 @@ def main(module=None):
     cf = cloudfiles.CloudFiles(swc_dir)
     files_exist = cf.exists([f"{spec_id}.swc" for spec_id in specimen_ids])
     for spec_id in specimen_ids:
-        # Load individual morphology
-        swc_file = f"{spec_id}.swc"
-        if not files_exist[swc_file]:
-            print(f"No SWC file found in {swc_dir} for {spec_id}")
-            continue
-        swc_file = os.path.join(swc_dir, swc_file)
-        morph_df = load_swc_as_dataframe(swc_file)
+        try:
+            # Load individual morphology
+            swc_file = f"{spec_id}.swc"
+            if not files_exist[swc_file]:
+                print(f"No SWC file found in {swc_dir} for {spec_id}")
+                continue
+            swc_file = os.path.join(swc_dir, swc_file)
+            morph_df = load_swc_as_dataframe(swc_file)
 
-        # Create the layer-aligned histogram
-        result = {}
-        node_types = [2, 3, 4]
-        for nt in node_types:
-            subset_df = morph_df.loc[morph_df["type"] == nt, :]
-            y_values = -subset_df["y"].values
-            hist_values, _ = np.histogram(y_values, bins=bins)
-            result[nt] = hist_values
+            # Create the layer-aligned histogram
+            result = {}
+            node_types = [2, 3, 4]
+            for nt in node_types:
+                subset_df = morph_df.loc[morph_df["type"] == nt, :]
+                y_values = -subset_df["y"].values
+                hist_values, _ = np.histogram(y_values, bins=bins)
+                result[nt] = hist_values
 
-        flat_result_dict = {"specimen_id": spec_id}
-        for nt in result:
-            for i in range(len(result[nt])):
-                k = f"{nt}_{i}"
-                flat_result_dict[k] = result[nt][i]
-        hist_record_list.append(flat_result_dict)
-
-        # Pull out the soma depths
-        soma_y = morph_df.loc[morph_df["type"] == 1, "y"].values[0]
-        soma_record_list.append(
-            {
-                "specimen_id": spec_id,
-                "soma_distance_from_pia": -soma_y,
-            }
-        )
+            flat_result_dict = {"specimen_id": spec_id}
+            for nt in result:
+                for i in range(len(result[nt])):
+                    k = f"{nt}_{i}"
+                    flat_result_dict[k] = result[nt][i]
+            # Pull out the soma depths
+            soma_y = morph_df.loc[morph_df["type"] == 1, "y"].values[0]
+            soma_record_list.append(
+                {
+                    "specimen_id": spec_id,
+                    "soma_distance_from_pia": -soma_y,
+                }
+            )
+            hist_record_list.append(flat_result_dict)
+        except:
+            print(f"Error processing {spec_id}")
 
     # Save results
     output_hist_file = module.args["output_hist_file"]

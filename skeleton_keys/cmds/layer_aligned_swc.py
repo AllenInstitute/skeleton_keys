14e322311f5aefa6ee81f3f9fb843f939cb562e6
Matt Mallory <matt.mallory@alleninstitute.org>
2024-02-19 16:10:01 -0800

    add: correct full-morph files in upright/alignment scripts

--- skeleton_keys/cmds/layer_aligned_swc.py
+++ skeleton_keys/cmds/layer_aligned_swc.py
@@ -3,6 +3,10 @@ import logging
 import numpy as np
 import pandas as pd
 import argschema as ags
+from skeleton_keys.layer_alignment import (
+    layer_aligned_y_values_for_morph,
+    cortex_thickness_aligned_y_values_for_morph,
+)
 from neuron_morphology.swc_io import morphology_from_swc, morphology_to_swc
 from neuron_morphology.transforms.pia_wm_streamlines.calculate_pia_wm_streamlines import (
     run_streamlines,
@@ -32,15 +36,12 @@ from skeleton_keys.drawings import (
 )
 from skeleton_keys.upright import corrected_without_uprighting_morph
 from skeleton_keys.io import read_json_file,load_default_layer_template
-from skeleton_keys.layer_alignment import (
-    layer_aligned_y_values_for_morph,
-    cortex_thickness_aligned_y_values_for_morph,
-)
 from skeleton_keys import cloudfields
+from skeleton_keys.full_morph import correct_upright_morph_orientation
 
 
 class LayerAlignedSwcSchema(ags.ArgSchema):
-    specimen_id = ags.fields.Integer(description="Specimen ID")
+    specimen_id = ags.fields.String(description="Specimen ID")
     swc_path = cloudfields.InputFile(
         description="path to SWC file (optional)", default=None, allow_none=True
     )
@@ -75,6 +76,11 @@ class LayerAlignedSwcSchema(ags.ArgSchema):
         default=["Layer1", "Layer2/3", "Layer4", "Layer5", "Layer6a", "Layer6b"],
         cli_as_single_argument=True,
     )
+    ccf_slice_orientation_correction = ags.fields.Boolean(
+        default=True,
+        description="When skeleton_keys.cmds.full_morph_layer_drawings was used to generate -swc_path and -surface_and_layers_file this will rotate the upright morphology about the soma-centered y-axis so that the result is near coronal orientation."
+    )
+
 
 
 def main(args):
@@ -83,6 +89,7 @@ def main(args):
     specimen_id = args["specimen_id"]
     swc_path = args["swc_path"]
     if swc_path is None:
+        specimen_id = int(specimen_id)
         swc_path = swc_paths_from_database([specimen_id])[specimen_id]
 
     # Load the reference layer depths
@@ -96,14 +103,18 @@ def main(args):
 
     # Get pia, white matter, soma, and layers
     surface_and_layers_file = args["surface_and_layers_file"]
+    atlas_slice_orient_data = None
     if surface_and_layers_file is not None:
         surfaces_and_paths = read_json_file(surface_and_layers_file)
         pia_surface = surfaces_and_paths["pia_path"]
         wm_surface = surfaces_and_paths["wm_path"]
         soma_drawing = surfaces_and_paths["soma_path"]
         layer_polygons = surfaces_and_paths["layer_polygons"]
+        if "atlas_slice_orientation" in surfaces_and_paths:
+            atlas_slice_orient_data = surfaces_and_paths['atlas_slice_orientation']
     else:
         # Query for image series ID
+        specimen_id = int(specimen_id)
         imser_id = query_for_image_series_id(specimen_id)
 
         # Query for pia, white matter, and soma
@@ -233,6 +244,10 @@ def main(args):
         parent_id_cb=lambda node: node["parent"],
     )
 
+    # rotate full_morph generated cell so it is near coronal orientation
+    if args['ccf_slice_orientation_correction'] and atlas_slice_orient_data is not None:
+        morph = correct_upright_morph_orientation(morph, atlas_slice_orient_data)
+      
     # save to swc
     output_file = args["output_file"]
     morphology_to_swc(aligned_morph, output_file)
--- skeleton_keys/cmds/upright_corrected_swc.py
+++ skeleton_keys/cmds/upright_corrected_swc.py
@@ -2,6 +2,10 @@ import shapely
 import numpy as np
 import pandas as pd
 import argschema as ags
+from skeleton_keys.layer_alignment import (
+    setup_interpolator_without_nan,
+    path_dist_from_node,
+)
 from neuron_morphology.swc_io import morphology_from_swc, morphology_to_swc
 from neuron_morphology.transforms.pia_wm_streamlines.calculate_pia_wm_streamlines import (
     run_streamlines,
@@ -28,16 +32,12 @@ from skeleton_keys.drawings import (
     convert_and_translate_snapped_to_microns,
     remove_duplicate_coordinates_from_drawings,
 )
-from skeleton_keys.layer_alignment import (
-    setup_interpolator_without_nan,
-    path_dist_from_node,
-)
 from skeleton_keys.io import read_json_file, load_default_layer_template
 from skeleton_keys import cloudfields
-
+from skeleton_keys.full_morph import correct_upright_morph_orientation
 
 class UprightCorrectedSwcSchema(ags.ArgSchema):
-    specimen_id = ags.fields.Integer(description="Specimen ID")
+    specimen_id = ags.fields.String(description="Specimen ID")
     swc_path = cloudfields.InputFile(
         description="path to SWC file (optional)", default=None, allow_none=True
     )
@@ -65,7 +65,10 @@ class UprightCorrectedSwcSchema(ags.ArgSchema):
         allow_none=True,
         description="Surface paths (streamlines) HDF5 file for slice angle calculation",
     )
-
+    ccf_slice_orientation_correction = ags.fields.Boolean(
+        default=True,
+        description="When skeleton_keys.cmds.full_morph_layer_drawings was used to generate swc_path and surface_and_layers_file this will rotate the upright morphology about the y-axis so that the result is near coronal orientation."
+    )
 
 def soma_distance_from_pia(pia_surface, depth_field, gradient_field, translation):
     """Calculate the distance (in microns) from the soma to the pia"""
@@ -111,17 +114,22 @@ def main(args):
     specimen_id = args["specimen_id"]
     swc_path = args["swc_path"]
     if swc_path is None:
+        specimen_id = int(specimen_id)
         swc_path = swc_paths_from_database([specimen_id])[specimen_id]
 
     # Get pia, white matter, soma, and layers
     surface_and_layers_file = args["surface_and_layers_file"]
+    atlas_slice_orient_data = None
     if surface_and_layers_file is not None:
         surfaces_and_paths = read_json_file(surface_and_layers_file)
         pia_surface = surfaces_and_paths["pia_path"]
         wm_surface = surfaces_and_paths["wm_path"]
         soma_drawing = surfaces_and_paths["soma_path"]
+        if "atlas_slice_orientation" in surfaces_and_paths:
+            atlas_slice_orient_data = surfaces_and_paths['atlas_slice_orientation']
     else:
         # Query for image series ID
+        specimen_id = int(specimen_id)
         imser_id = query_for_image_series_id(specimen_id)
 
         # Query for pia, white matter, and soma
@@ -195,6 +203,10 @@ def main(args):
     T_translate = AffineTransform(translation_affine)
     T_translate.transform_morphology(morph)
 
+    # rotate full_morph generated cell so it is near coronal orientation
+    if args['ccf_slice_orientation_correction'] and atlas_slice_orient_data is not None:
+        morph = correct_upright_morph_orientation(morph, atlas_slice_orient_data)
+        
     # save to swc
     output_file = args["output_file"]
     morphology_to_swc(morph, output_file)
